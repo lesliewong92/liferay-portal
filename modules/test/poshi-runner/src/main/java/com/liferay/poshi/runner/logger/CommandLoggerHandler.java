596d2b6c418eeb5f68efa15cbcd8f313c3641723
Leslie Wong <leslie.wong@liferay.com>
2015-10-21 15:16:15 -0700

    LRQA-18727 Escape HTML in command log

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/logger/CommandLoggerHandler.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/logger/CommandLoggerHandler.java
@@ -19,6 +19,7 @@ import com.liferay.poshi.runner.PoshiRunnerGetterUtil;
 import com.liferay.poshi.runner.PoshiRunnerStackTraceUtil;
 import com.liferay.poshi.runner.PoshiRunnerVariablesUtil;
 import com.liferay.poshi.runner.selenium.LiferaySeleniumHelper;
+import com.liferay.poshi.runner.util.HtmlUtil;
 import com.liferay.poshi.runner.util.StringUtil;
 import com.liferay.poshi.runner.util.Validator;
 
@@ -257,7 +258,9 @@ public final class CommandLoggerHandler {
 				String paramValue =
 					PoshiRunnerVariablesUtil.getValueFromExecuteMap(locatorKey);
 
-				sb.append(_getLineItemText("param-value", paramValue));
+				sb.append(
+					_getLineItemText(
+						"param-value", HtmlUtil.escape(paramValue)));
 			}
 
 			String valueKey = "value" + (i + 1);
@@ -269,7 +272,9 @@ public final class CommandLoggerHandler {
 				String paramValue =
 					PoshiRunnerVariablesUtil.getValueFromExecuteMap(valueKey);
 
-				sb.append(_getLineItemText("param-value", paramValue));
+				sb.append(
+					_getLineItemText(
+						"param-value", HtmlUtil.escape(paramValue)));
 			}
 		}
 
@@ -380,7 +385,8 @@ public final class CommandLoggerHandler {
 			sb.append(_getLineItemText("misc", " with parameters"));
 
 			for (String argument : arguments) {
-				sb.append(_getLineItemText("param-value", argument));
+				sb.append(
+					_getLineItemText("param-value", HtmlUtil.escape(argument)));
 			}
 		}
 
