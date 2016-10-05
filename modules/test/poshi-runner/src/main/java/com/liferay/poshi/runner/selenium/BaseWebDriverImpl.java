324af0df50631667c6d8e9b7cdfe24b745ed6c20
Anthony Chu <anthony.chu@liferay.com>
2016-10-05 12:06:26 -0700

    LRQA-28184 Preserve continuity across newlines for script editor fields

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
@@ -1369,7 +1369,12 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 
 		text = text.trim();
 
-		return text.replace("\n", " ");
+		if (locator.contains("Editor")) {
+			return text.replace("\n", "");
+		}
+		else {
+			return text.replace("\n", " ");
+		}
 	}
 
 	@Override
