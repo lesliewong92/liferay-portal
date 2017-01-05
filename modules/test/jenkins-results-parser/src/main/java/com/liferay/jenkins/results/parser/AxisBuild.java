6e97e5dc0e3dc9b1a257e0a8a4f3d58e14df2b4d
Peter Yoo <peter.yoo@liferay.com>
2017-01-05 09:12:59 -0800

    LRQA-29637 "throws Exception" is unnecessary.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AxisBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AxisBuild.java
@@ -39,7 +39,7 @@ public class AxisBuild extends BaseBuild {
 	}
 
 	@Override
-	public String getAppServer() throws Exception {
+	public String getAppServer() {
 		Build parentBuild = getParentBuild();
 
 		return parentBuild.getAppServer();
@@ -85,7 +85,7 @@ public class AxisBuild extends BaseBuild {
 	}
 
 	@Override
-	public String getBrowser() throws Exception {
+	public String getBrowser() {
 		Build parentBuild = getParentBuild();
 
 		return parentBuild.getBrowser();
@@ -151,13 +151,12 @@ public class AxisBuild extends BaseBuild {
 	}
 
 	@Override
-	public String getDatabase() throws Exception {
+	public String getDatabase() {
 		Build parentBuild = getParentBuild();
 
 		return parentBuild.getDatabase();
 	}
 
-	@Override
 	public String getDisplayName() {
 		StringBuilder sb = new StringBuilder();
 
@@ -169,14 +168,14 @@ public class AxisBuild extends BaseBuild {
 	}
 
 	@Override
-	public String getJavaJDK() throws Exception {
+	public String getJavaJDK() {
 		Build parentBuild = getParentBuild();
 
 		return parentBuild.getJavaJDK();
 	}
 
 	@Override
-	public String getOperatingSystem() throws Exception {
+	public String getOperatingSystem() {
 		Build parentBuild = getParentBuild();
 
 		return parentBuild.getOperatingSystem();
