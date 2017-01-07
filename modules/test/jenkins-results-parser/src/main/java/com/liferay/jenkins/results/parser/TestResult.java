61e3e345818df1a61903981398f286b58221e3bc
Leslie Wong <leslie.wong@liferay.com>
2017-01-06 17:46:09 -0800

    LRQA-29637 Calculate environment hash in test results

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
@@ -18,6 +18,8 @@ import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -187,6 +189,45 @@ public class TestResult {
 		}
 
 		return !liferayLog.isEmpty();
+	}
+
+	protected String getEnvironmentHash(Properties properties)
+		throws Exception {
+
+		List<Map<String, Object>> environmentOptions = DBUtil.executeQuery(
+			new ArrayList<>(),
+			properties.getProperty("testray.integration.database.type"),
+			properties.getProperty("testray.integration.database.password"),
+			properties.getProperty("testray.integration.environment.query"),
+			properties.getProperty("testray.integration.database.url"),
+			properties.getProperty("testray.integration.database.user.name"));
+
+		List<String> environments = new ArrayList<>();
+
+		environments.add(axisBuild.getAppServer());
+		environments.add(axisBuild.getBrowser());
+		environments.add(axisBuild.getDatabase());
+		environments.add(axisBuild.getJDK());
+		environments.add(axisBuild.getOperatingSystem());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (String environment : environments) {
+			for (Map<String, Object> environmentOption : environmentOptions) {
+				String name = (String)environmentOption.get("name");
+
+				if (environment.equals(name)) {
+					sb.append(environmentOption.get("testrayFactorCategoryId"));
+					sb.append(environmentOption.get("testrayFactorOptionId"));
+
+					break;
+				}
+			}
+		}
+
+		String testrayFactors = sb.toString();
+
+		return String.valueOf(testrayFactors.hashCode());
 	}
 
 	protected AxisBuild axisBuild;
