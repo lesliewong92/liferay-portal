91c254e955041f2a99b8bd8caef22e2a99cfe454
Leslie Wong <leslie.wong@liferay.com>
2017-01-06 17:50:53 -0800

    LRQA-29637 Calculate percentage of failure in test history

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
@@ -16,10 +16,13 @@ package com.liferay.jenkins.results.parser;
 
 import java.io.IOException;
 
+import java.sql.Date;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -191,6 +194,94 @@ public class TestResult {
 		return !liferayLog.isEmpty();
 	}
 
+	protected void calculateFailurePercentage() throws Exception {
+		Properties buildProperties;
+
+		try {
+			buildProperties = JenkinsResultsParserUtil.getBuildProperties();
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException("Unable to get build.properties.", ioe);
+		}
+
+		List<Object> arguments = new ArrayList<>();
+
+		arguments.add(simpleClassName + "#" + testName);
+
+		String dbDriverClassName = buildProperties.getProperty(
+			"testray.integration.database.type");
+		String password = buildProperties.getProperty(
+			"testray.integration.database.password");
+		String url = buildProperties.getProperty(
+			"testray.integration.database.url");
+		String userName = buildProperties.getProperty(
+			"testray.integration.database.user.name");
+
+		List<Map<String, Object>> testCaseIDs = DBUtil.executeQuery(
+			arguments, dbDriverClassName, password,
+			buildProperties.getProperty(
+				"testray.integration.testcase.id.query"),
+			url, userName);
+
+		Map<String, Object> testCaseId = testCaseIDs.get(0);
+
+		long testrayCaseId = (Long)testCaseId.get("testrayCaseId");
+
+		arguments.clear();
+
+		arguments.add(buildProperties.getProperty("testray.build.type"));
+
+		List<Map<String, Object>> buildTypes = DBUtil.executeQuery(
+			arguments, dbDriverClassName, password,
+			buildProperties.getProperty(
+				"testray.integration.build.type.id.query"),
+			url, userName);
+
+		Map<String, Object> buildType = buildTypes.get(0);
+
+		long buildTypeId = (Long)buildType.get("testrayBuildTypeId");
+
+		arguments.clear();
+
+		arguments.add(testrayCaseId);
+		arguments.add(buildTypeId);
+		arguments.add(getEnvironmentHash(buildProperties));
+
+		long durationInMillis = TimeUnit.DAYS.toMillis(
+			Long.valueOf(
+				buildProperties.getProperty("flakiness.assertion.duration")));
+
+		arguments.add(new Date(System.currentTimeMillis() - durationInMillis));
+
+		List<Map<String, Object>> statuses = DBUtil.executeQuery(
+			arguments, dbDriverClassName, password,
+			buildProperties.getProperty("testray.integration.status.query"),
+			url, userName);
+
+		if (statuses.isEmpty()) {
+			failurePercentage = 0;
+
+			return;
+		}
+
+		int total = 0;
+		int failureCount = 0;
+
+		for (Map<String, Object> status : statuses) {
+			Long count = (Long)status.get("COUNT(*)");
+
+			int statusCount = count.intValue();
+
+			if ((Integer)status.get("status") == STATUS_FAILED) {
+				failureCount = statusCount;
+			}
+
+			total += statusCount;
+		}
+
+		failurePercentage = ((double)failureCount / (double)total) * 100;
+	}
+
 	protected String getEnvironmentHash(Properties properties)
 		throws Exception {
 
@@ -230,9 +321,12 @@ public class TestResult {
 		return String.valueOf(testrayFactors.hashCode());
 	}
 
+	protected static final int STATUS_FAILED = 3;
+
 	protected AxisBuild axisBuild;
 	protected String className;
 	protected long duration;
+	protected double failurePercentage = -1.0;
 	protected String packageName;
 	protected String simpleClassName;
 	protected String status;
