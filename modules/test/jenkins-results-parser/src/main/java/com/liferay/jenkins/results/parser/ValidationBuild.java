1e19cc56f36d515c7f4c8f9f76fb54182a450035
Leslie Wong <leslie.wong@liferay.com>
2017-10-16 12:28:26 -0700

    LRQA-TEMP Add link to full console

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/ValidationBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/ValidationBuild.java
@@ -68,6 +68,13 @@ public class ValidationBuild extends BaseBuild {
 					getTaskSummaryIndexElement(consoleSnippet));
 			}
 
+			Element consoleLinkElement = Dom4JUtil.getNewElement(
+				"h5", null, "For full console, click ",
+				Dom4JUtil.getNewAnchorElement(
+					getBuildURL() + "/consoleText", "here"), ".");
+
+			Dom4JUtil.addToElement(rootElement, consoleLinkElement);
+
 			Dom4JUtil.addToElement(
 				rootElement, Dom4JUtil.getNewElement("hr"),
 				getTestSummaryElement());
