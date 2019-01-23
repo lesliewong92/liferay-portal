6d16a0a66da5d502d0c51822e901ddcbcddfe186
Leslie Wong <leslie.wong@liferay.com>
2019-01-23 15:02:43 -0800

    LRQA-TEMP Rename to 'EventSender'

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/EventSender.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Leslie Wong
+ */
+public class EventSender {
+
+	protected void notify(String eventName, Build build) {
+		List<EventListener> eventListenerList = _eventListeners.get(eventName);
+
+		for (EventListener eventListener : eventListenerList) {
+			eventListener.update(build);
+		}
+	}
+
+	protected void subscribe(String eventName, EventListener eventListener) {
+		List<EventListener> eventListenerList = _eventListeners.get(eventName);
+
+		if (eventListenerList == null) {
+			eventListenerList = new ArrayList<>();
+		}
+
+		eventListenerList.add(eventListener);
+
+		_eventListeners.put(eventName, eventListenerList);
+	}
+
+	protected void unsubscribe(String eventName, EventListener eventListener) {
+		List<EventListener> eventListenerList = _eventListeners.get(eventName);
+
+		if (eventListenerList != null) {
+			eventListenerList.remove(eventListener);
+
+			_eventListeners.put(eventName, eventListenerList);
+		}
+	}
+
+	private Map<String, List<EventListener>> _eventListeners = new HashMap<>();
+
+}
