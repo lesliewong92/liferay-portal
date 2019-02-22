7d9047b79aa191beeb403b4b4fc221da59a6fbb4
Leslie Wong <leslie.wong@liferay.com>
2019-02-21 18:35:01 -0800

    LRQA-TEMP Do a more extensive check for modified downstream builds

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
@@ -1175,7 +1175,9 @@ public abstract class BaseBuild implements Build {
 	@Override
 	public boolean hasModifiedDownstreamBuilds() {
 		for (Build downstreamBuild : downstreamBuilds) {
-			if (downstreamBuild.isBuildModified()) {
+			if (downstreamBuild.isBuildModified() ||
+				downstreamBuild.hasModifiedDownstreamBuilds()) {
+
 				return true;
 			}
 		}
@@ -1356,7 +1358,8 @@ public abstract class BaseBuild implements Build {
 	public void update() {
 		String status = getStatus();
 
-		if ((status.equals("completed") && isBuildModified()) ||
+		if ((status.equals("completed") &&
+			 (isBuildModified() || hasModifiedDownstreamBuilds())) ||
 			!status.equals("completed")) {
 
 			_previousStatus = _status;
