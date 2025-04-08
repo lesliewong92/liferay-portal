c3422ef0a91d27548d4528e18b69dbc45d5db89e
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-04-08 01:36:33 -0700

    LPD-52246 Avoid calling getSegmentsEntryIds if values is in request cache

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
@@ -69,6 +69,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.segments.SegmentsEntryRetriever;
+import com.liferay.segments.constants.SegmentsWebKeys;
 import com.liferay.segments.context.RequestContextMapper;
 
 import java.util.ArrayList;
@@ -1087,15 +1088,23 @@ public class RenderLayoutStructureDisplayContext {
 			return _segmentsEntryIds;
 		}
 
-		SegmentsEntryRetriever segmentsEntryRetriever =
-			ServletContextUtil.getSegmentsEntryRetriever();
+		long[] cachedSegmentEntryIds = (long[])_httpServletRequest.getAttribute(
+			SegmentsWebKeys.SEGMENTS_ENTRY_IDS);
 
-		RequestContextMapper requestContextMapper =
-			ServletContextUtil.getRequestContextMapper();
+		if (cachedSegmentEntryIds != null) {
+			_segmentsEntryIds = cachedSegmentEntryIds;
+		}
+		else {
+			SegmentsEntryRetriever segmentsEntryRetriever =
+				ServletContextUtil.getSegmentsEntryRetriever();
 
-		_segmentsEntryIds = segmentsEntryRetriever.getSegmentsEntryIds(
-			_themeDisplay.getScopeGroupId(), _themeDisplay.getUserId(),
-			requestContextMapper.map(_httpServletRequest), new long[0]);
+			RequestContextMapper requestContextMapper =
+				ServletContextUtil.getRequestContextMapper();
+
+			_segmentsEntryIds = segmentsEntryRetriever.getSegmentsEntryIds(
+				_themeDisplay.getScopeGroupId(), _themeDisplay.getUserId(),
+				requestContextMapper.map(_httpServletRequest), new long[0]);
+		}
 
 		return _segmentsEntryIds;
 	}
