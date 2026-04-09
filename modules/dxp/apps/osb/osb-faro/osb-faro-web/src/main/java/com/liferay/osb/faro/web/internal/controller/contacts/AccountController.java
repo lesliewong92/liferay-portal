1b34e249bcd41000339e830843a2d34046de5342
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2026-04-09 15:18:39 -0700

    LPD-84237 Remove unused endpoints and update search endpoint

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/contacts/AccountController.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/contacts/AccountController.java
@@ -5,7 +5,6 @@
 
 package com.liferay.osb.faro.web.internal.controller.contacts;
 
-import com.liferay.osb.faro.contacts.model.constants.JSONConstants;
 import com.liferay.osb.faro.engine.client.model.Account;
 import com.liferay.osb.faro.engine.client.model.Field;
 import com.liferay.osb.faro.engine.client.model.Results;
@@ -13,25 +12,21 @@ import com.liferay.osb.faro.engine.client.util.OrderByField;
 import com.liferay.osb.faro.web.internal.constants.FaroConstants;
 import com.liferay.osb.faro.web.internal.controller.BaseFaroController;
 import com.liferay.osb.faro.web.internal.controller.FaroController;
+import com.liferay.osb.faro.web.internal.model.display.FaroFDSResultsDisplay;
 import com.liferay.osb.faro.web.internal.model.display.FaroResultsDisplay;
 import com.liferay.osb.faro.web.internal.model.display.contacts.AccountDisplay;
 import com.liferay.osb.faro.web.internal.param.FaroParam;
-import com.liferay.osb.faro.web.internal.search.FaroSearchContext;
 import com.liferay.osb.faro.web.internal.util.PhotoURLHelper;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.RoleConstants;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 
 import javax.annotation.security.RolesAllowed;
 
 import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,7 +34,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Matthew Kong
@@ -60,19 +54,6 @@ public class AccountController extends BaseFaroController {
 			contactsEngineClient.getAccount(
 				faroProjectLocalService.getFaroProjectByGroupId(groupId), id),
 			_photoURLHelper);
-	}
-
-	@GET
-	@Path("/{id}/details")
-	@RolesAllowed(RoleConstants.SITE_MEMBER)
-	public Map<String, List<Field>> getDetails(
-			@PathParam("groupId") long groupId, @PathParam("id") String id)
-		throws Exception {
-
-		Account account = contactsEngineClient.getAccount(
-			faroProjectLocalService.getFaroProjectByGroupId(groupId), id);
-
-		return account.getOrganization();
 	}
 
 	@GET
@@ -103,64 +84,25 @@ public class AccountController extends BaseFaroController {
 		return _ENTITY_TYPES.clone();
 	}
 
-	@Override
-	public FaroResultsDisplay search(
-			long groupId, FaroSearchContext faroSearchContext)
-		throws Exception {
-
-		return search(
-			groupId, null, null, null, null, faroSearchContext.getQuery(),
-			Collections.emptyList(), faroSearchContext.getCur(),
-			faroSearchContext.getDelta(), faroSearchContext.getOrderByFields());
-	}
-
 	@GET
+	@Path("/search")
 	@RolesAllowed(RoleConstants.SITE_MEMBER)
-	public FaroResultsDisplay search(
+	public FaroFDSResultsDisplay search(
 			@PathParam("groupId") long groupId,
 			@QueryParam("channelId") String channelId,
-			@QueryParam("dataSourceId") String dataSourceId,
-			@QueryParam("individualSegmentId") String individualSegmentId,
 			@QueryParam("filter") String filterString,
+			@QueryParam("page") int page, @QueryParam("pageSize") int pageSize,
 			@QueryParam("query") String query,
-			@DefaultValue(JSONConstants.NULL_JSON_ARRAY)
-			@QueryParam("includePropertyNames")
-			FaroParam
-				<List<String>> includePropertyNamesFaroParam,
-			@QueryParam("cur") int cur, @QueryParam("delta") int delta,
-			@DefaultValue(StringPool.BLANK) @QueryParam("orderByFields")
-				FaroParam<List<OrderByField>> orderByFieldsFaroParam)
+			@DefaultValue(StringPool.BLANK) @QueryParam("sort") String sort)
 		throws Exception {
 
-		return search(
-			groupId, channelId, dataSourceId, individualSegmentId, filterString,
-			query, includePropertyNamesFaroParam.getValue(), cur, delta,
-			orderByFieldsFaroParam.getValue());
-	}
+		Results<Account> results = contactsEngineClient.getAccounts(
+			faroProjectLocalService.getFaroProjectByGroupId(groupId), channelId,
+			filterString, query, page, pageSize, sort);
 
-	@Path("/search")
-	@POST
-	@RolesAllowed(RoleConstants.SITE_MEMBER)
-	public FaroResultsDisplay searchByForm(
-			@PathParam("groupId") long groupId,
-			@FormParam("channelId") String channelId,
-			@FormParam("dataSourceId") String dataSourceId,
-			@FormParam("individualSegmentId") String individualSegmentId,
-			@FormParam("filter") String filterString,
-			@FormParam("query") String query,
-			@DefaultValue(JSONConstants.NULL_JSON_ARRAY)
-			@FormParam("includePropertyNames")
-			FaroParam
-				<List<String>> includePropertyNamesFaroParam,
-			@FormParam("cur") int cur, @FormParam("delta") int delta,
-			@DefaultValue(StringPool.BLANK) @FormParam("orderByFields")
-				FaroParam<List<OrderByField>> orderByFieldsFaroParam)
-		throws Exception {
+		Function<Account, AccountDisplay> function = AccountDisplay::new;
 
-		return search(
-			groupId, channelId, dataSourceId, individualSegmentId, filterString,
-			query, includePropertyNamesFaroParam.getValue(), cur, delta,
-			orderByFieldsFaroParam.getValue());
+		return new FaroFDSResultsDisplay(results, function, page, pageSize);
 	}
 
 	@GET
@@ -179,31 +121,6 @@ public class AccountController extends BaseFaroController {
 			contactsEngineClient.getAccountFieldValues(
 				faroProjectLocalService.getFaroProjectByGroupId(groupId),
 				channelId, fieldMappingFieldName, query, cur, delta));
-	}
-
-	@SuppressWarnings("unchecked")
-	protected FaroResultsDisplay search(
-			long groupId, String channelId, String dataSourceId,
-			String individualSegmentId, String filterString, String query,
-			List<String> includePropertyNames, int cur, int delta,
-			List<OrderByField> orderByFields)
-		throws Exception {
-
-		Results<Account> results = contactsEngineClient.getAccounts(
-			faroProjectLocalService.getFaroProjectByGroupId(groupId), channelId,
-			dataSourceId, individualSegmentId, filterString, query,
-			Collections.singletonList("accountName"), cur, delta,
-			orderByFields);
-
-		Function<Account, AccountDisplay> function = account -> {
-			AccountDisplay accountDisplay = new AccountDisplay(account, null);
-
-			accountDisplay.addProperties(includePropertyNames);
-
-			return accountDisplay;
-		};
-
-		return new FaroResultsDisplay(results, function);
 	}
 
 	private static final int[] _ENTITY_TYPES = {FaroConstants.TYPE_ACCOUNT};
