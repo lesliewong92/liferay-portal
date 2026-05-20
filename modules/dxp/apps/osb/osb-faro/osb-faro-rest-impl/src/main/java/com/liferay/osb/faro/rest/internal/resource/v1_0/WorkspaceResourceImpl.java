ed64937b7b9548e7559bbd32776e693aa9f0e048
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2026-05-19 23:29:36 -0700

    LPD-91204 Implement the workspaces resource

--- modules/dxp/apps/osb/osb-faro/osb-faro-rest-impl/src/main/java/com/liferay/osb/faro/rest/internal/resource/v1_0/WorkspaceResourceImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-rest-impl/src/main/java/com/liferay/osb/faro/rest/internal/resource/v1_0/WorkspaceResourceImpl.java
@@ -5,9 +5,16 @@
 
 package com.liferay.osb.faro.rest.internal.resource.v1_0;
 
+import com.liferay.osb.faro.constants.FaroUserConstants;
+import com.liferay.osb.faro.model.FaroProject;
+import com.liferay.osb.faro.rest.dto.v1_0.Workspace;
 import com.liferay.osb.faro.rest.resource.v1_0.WorkspaceResource;
+import com.liferay.osb.faro.service.FaroProjectLocalService;
+import com.liferay.osb.faro.service.FaroUserLocalService;
+import com.liferay.portal.vulcan.pagination.Page;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +25,35 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WorkspaceResource.class
 )
 public class WorkspaceResourceImpl extends BaseWorkspaceResourceImpl {
+
+	@Override
+	public Page<Workspace> getWorkspacesPage() throws Exception {
+		return Page.of(
+			transform(
+				_faroUserLocalService.getFaroUsersByLiveUserId(
+					contextUser.getUserId(), FaroUserConstants.STATUS_APPROVED),
+				faroUser -> {
+					FaroProject faroProject =
+						_faroProjectLocalService.fetchFaroProjectByGroupId(
+							faroUser.getGroupId());
+
+					if (faroProject == null) {
+						return null;
+					}
+
+					return new Workspace() {
+						{
+							setGroupId(faroProject::getGroupId);
+							setName(faroProject::getName);
+						}
+					};
+				}));
+	}
+
+	@Reference
+	private FaroProjectLocalService _faroProjectLocalService;
+
+	@Reference
+	private FaroUserLocalService _faroUserLocalService;
+
 }
