68e416ee223770630f630c92987a6c22ecc48369
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2026-06-15 22:54:47 -0700

    LPD-92420 Add test

--- modules/apps/analytics/analytics-cms-rest-test/src/testIntegration/java/com/liferay/analytics/cms/rest/resource/v1_0/test/ExportResourceTest.java
+++ modules/apps/analytics/analytics-cms-rest-test/src/testIntegration/java/com/liferay/analytics/cms/rest/resource/v1_0/test/ExportResourceTest.java
@@ -5,15 +5,150 @@
 
 package com.liferay.analytics.cms.rest.resource.v1_0.test;
 
+import com.liferay.analytics.cms.rest.resource.v1_0.ExportResource;
+import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.depot.constants.DepotConstants;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.petra.function.UnsafeSupplier;
+import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.MockHttp;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
-import org.junit.Ignore;
+import jakarta.ws.rs.core.Response;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Rachael Koestartyo
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class ExportResourceTest extends BaseExportResourceTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_depotEntry = _depotEntryLocalService.addDepotEntry(
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
+			DepotConstants.TYPE_ASSET_LIBRARY,
+			ServiceContextTestUtil.getServiceContext(
+				testGroup.getGroupId(), TestPropsValues.getUserId()));
+	}
+
+	@Override
+	@Test
+	public void testGetCategoryExport() throws Exception {
+		_testGetExport(
+			"category,count\nNews,5", "categories.csv", "/categories/export",
+			() -> _exportResource.getCategoryExport(
+				new Long[] {_depotEntry.getDepotEntryId()},
+				RandomTestUtil.nextInt()));
+	}
+
+	@Override
+	@Test
+	public void testGetGeolocationExport() throws Exception {
+		_testGetExport(
+			"country,visits\nUS,10", "geolocation.csv", "/geolocation/export",
+			() -> _exportResource.getGeolocationExport(
+				new Long[] {_depotEntry.getDepotEntryId()},
+				RandomTestUtil.nextInt()));
+	}
+
+	@Override
+	@Test
+	public void testGetSummaryExport() throws Exception {
+		_testGetExport(
+			"metric,value\nviews,20", "summaries.csv", "/summaries/export",
+			() -> _exportResource.getSummaryExport(
+				new Long[] {_depotEntry.getDepotEntryId()},
+				RandomTestUtil.nextInt()));
+	}
+
+	private void _testGetExport(
+			String content, String fileName, String path,
+			UnsafeSupplier<Response, Exception> unsafeSupplier)
+		throws Exception {
+
+		try (CompanyConfigurationTemporarySwapper
+				companyConfigurationTemporarySwapper =
+					new CompanyConfigurationTemporarySwapper(
+						testCompany.getCompanyId(),
+						AnalyticsConfiguration.class.getName(),
+						HashMapDictionaryBuilder.<String, Object>put(
+							"liferayAnalyticsDataSourceId",
+							RandomTestUtil.nextLong()
+						).put(
+							"liferayAnalyticsEnableAllGroupIds", true
+						).put(
+							"liferayAnalyticsFaroBackendSecuritySignature",
+							RandomTestUtil.randomString()
+						).put(
+							"liferayAnalyticsFaroBackendURL",
+							"http://" + RandomTestUtil.randomString()
+						).build())) {
+
+			ReflectionTestUtil.setFieldValue(
+				_exportResource, "_http",
+				new MockHttp(
+					Collections.singletonMap(
+						"/api/1.0/asset-metric/objectEntry" + path,
+						() -> content)));
+
+			Response response = unsafeSupplier.get();
+
+			Assert.assertEquals(200, response.getStatus());
+			Assert.assertEquals(content, response.getEntity());
+			Assert.assertEquals(
+				"attachment; filename=" + fileName,
+				response.getHeaderString("Content-Disposition"));
+		}
+		finally {
+			ReflectionTestUtil.setFieldValue(_exportResource, "_http", _http);
+		}
+	}
+
+	@DeleteAfterTestRun
+	private DepotEntry _depotEntry;
+
+	@Inject
+	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Inject
+	private ExportResource _exportResource;
+
+	@Inject
+	private Http _http;
+
 }
